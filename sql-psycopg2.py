dd667847ace9199c6375cbda54fc1d6c498abf94
struk49 <andrewstruk@yahoo.co.uk>
2023-06-10 15:12:58 +0000

    adeed sqlalchemy

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,10 +1,9 @@
 import psycopg2
 
-
 # connect to "chinook" database
-connection = psycopg2.connect(database="chinnook")
+connection = psycopg2.connect(database="chinook")
 
-# build a cursor object of the database
+# build a curser object of the database
 cursor = connection.cursor()
 
 # Query 1 - select all records from the "Artist" table
@@ -22,18 +21,19 @@ cursor = connection.cursor()
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
-# fetch the results (multiple)
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["AC/DC"])
+
+#fetch the results (multiple)
 results = cursor.fetchall()
 
-# fetch the result (single)
+# fetch the results (single)
 # results = cursor.fetchone()
 
 # close the connection
 connection.close()
 
-# print results
+#print results
 for result in results:
     print(result)
